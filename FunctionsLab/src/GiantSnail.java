a6a23eb6ec70243b9bc92a552a423312ebc98623
mjergens <mjergens@172.16.202.162>
2016-11-09 10:02:34 -0500

    Formatted GiantSnail javadoc

--- FunctionsLab/src/GiantSnail.java
+++ FunctionsLab/src/GiantSnail.java
@@ -1,57 +1,75 @@
 //take parameters
 
-/*
- * Part 3
+/**
+ * @author cforster
  * 
- * Expected output is:
+ *         Part 3
  * 
- *   The Giant Snails are invading Miami, Florida.
- *   They collected 78000 african land snails. 
- *   There are 1 snails per square foot in Florida.
- *   If that was rhode island, there would be 64 snails per square foot.
+ *         Expected output is:
+ * 
+ *         The Giant Snails are invading Miami, Florida.
+ * 
+ *         They collected 78000 african land snails.
+ * 
+ *         There are 1 snails per square foot in Florida.
+ * 
+ *         If that was rhode island, there would be 64 snails per square foot.
  * 
  */
 public class GiantSnail {
-	//DONT MODIFY THIS CODE:
-	public static void main(String[] args) {
-		invade("Miami, Florida");
-		count(78000); //snails
-		dense(78000, 65750); //snails, sq ft size of florida
-		dense(78000);
-	}
-	
-	//MODIFY STARTING HERE:
-	/**
-	 * prints an invasion of giant snails based on a location
-	 * @param location the location of the invasion
-	 */
-	static void invade(String location) {
-		
-	}
-	
-	/**
-	 * prints the number of snails collected
-	 * @param how_many the number of snails
-	 */
-	static void count(int how_many) {
-	
-	}
-	
-	/**
-	 * prints the density of giant snails
-	 * @param how_many the number of snails
-	 * @param how_large the size of the space they are in
-	 */
-	static void dense(double how_many, double how_large) {
-		
-	}
-	
-	/**
-	 * prints the density of the giant snails assuming they are in rhode island (1212sq feet):
-	 * @param how_many the number of snails 
-	 */
-	static void dense(double how_many)
-	{
-		
-	}	
+    // DONT MODIFY THIS CODE:
+    
+    /**
+     * @param args - command line input (not used)
+     */
+    public static void main(String[] args) {
+	invade("Miami, Florida");
+	count(78000); // snails
+	dense(78000, 65750); // snails, sq ft size of florida
+	dense(78000);
+    }
+
+    // MODIFY STARTING HERE:
+    /**
+     * prints an invasion of giant snails based on a location
+     * 
+     * @param location
+     *            the location of the invasion
+     */
+    static void invade(String location) {
+
+    }
+
+    /**
+     * prints the number of snails collected
+     * 
+     * @param how_many
+     *            the number of snails
+     */
+    static void count(int how_many) {
+
+    }
+
+    /**
+     * prints the density of giant snails
+     * 
+     * @param how_many
+     *            the number of snails
+     * @param how_large
+     *            the size of the space they are in
+     */
+    static void dense(double how_many, double how_large) {
+
+    }
+
+    /**
+     * prints the density of the giant snails assuming they are in rhode island
+     * (1212sq feet):
+     * 
+     * @param how_many
+     *            the number of snails
+     */
+    static void dense(double how_many) {
+
+    }
 }
