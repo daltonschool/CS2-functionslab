6d4e4cbf41fb6d95f9d227b4416aa5a3cc86986a
mjergens <mjergens@172.16.202.162>
2016-11-09 10:01:14 -0500

    Added javadoc comments to SeaCucumbers

--- FunctionsLab/src/SeaCucumbers.java
+++ FunctionsLab/src/SeaCucumbers.java
@@ -1,58 +1,78 @@
 import java.util.Random;
 
-/*
- * Part 2
+/**
+ * @author cforster
  * 
- * Expected Output:
+ *         Part 2
  * 
- *   The scientific name for the sea cucumber is Holothuroidea
- *   There are 1250 species of sea cucumber.
- *   My favorite type is the warty sea cucumber.
- *   I have 239 sea cucumbers in my pond.
- *   My favorite sea cucumber song is Ode to the Cucumber.
+ *         Expected Output:
+ * 
+ *         The scientific name for the sea cucumber is Holothuroidea
+ * 
+ *         There are 1250 species of sea cucumber.
+ * 
+ *         My favorite type is the warty sea cucumber.
+ * 
+ *         I have 239 sea cucumbers in my pond.
+ * 
+ *         My favorite sea cucumber song is Ode to the Cucumber.
  */
 public class SeaCucumbers {
-	//DONT MODIFY THIS CODE:
-	public static void main(String[] args) {
-		System.out.print("The scientific name for the sea cucumber is ");
-		science();
-		
-		System.out.print("There are " + species() + " species of sea cucumber.");
-		
-		System.out.println("My favorite type is the " + warty() + " sea cucumber.");
-		
-		System.out.println("I have " + rando().nextInt(500) + " sea cucumbers in my pond.");
-		
-		System.out.println("My favorite sea cucumber song is " + song()[2] + ".");
-	}
-	
-	//MODIFY THIS CODE:
-	static void science()
-	{
-		
-	}
-	
-	static int species()
-	{
-		return 0;
-	}
-	
-	static String warty()
-	{
-		return null;
-	}
-	
-	static Random rando()
-	{
-		return null;
-	}
-	
-	static String[] song()
-	{
-		return null;
-	}
+    // DONT MODIFY THIS CODE:
+    /**
+     * @param args
+     *            - command line input (not used)
+     */
+    public static void main(String[] args) {
+	System.out.print("The scientific name for the sea cucumber is ");
+	science();
+
+	System.out.print("There are " + species() + " species of sea cucumber.");
+
+	System.out.println("My favorite type is the " + warty() + " sea cucumber.");
+
+	System.out.println("I have " + rando().nextInt(500) + " sea cucumbers in my pond.");
+
+	System.out.println("My favorite sea cucumber song is " + song()[2] + ".");
+    }
+
+    /*
+     *  MODIFY THIS CODE:
+     */
+    
+    /**
+     * Display a scientific name.
+     */
+    static void science() {
+
+    }
+
+    /**
+     * @return - number of species
+     */
+    static int species() {
+	return 0;
+    }
+
+    /**
+     * @return - String
+     */
+    static String warty() {
+	return null;
+    }
+
+    /**
+     * @return - Random
+     */
+    static Random rando() {
+	return null;
+    }
+
+    /**
+     * @return - String array
+     */
+    static String[] song() {
+	return null;
+    }
 }
 
-//use functions
-//break a long main into functions
-//design some functions
